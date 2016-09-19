379f09fbfbb62ea2cc6ec82dbd62dfbb6fd359f8
palsingh <pal.singh1989@gmail.com>
2016-09-20 01:38:54 +0530

    Added responsiveness for iPhone 6

--- scripts/jsx/Cities.jsx
+++ scripts/jsx/Cities.jsx
@@ -1,18 +1,31 @@
 class Cities extends React.Component {
 
-	changeLocation(city) {
+	changeLocationSelect (e) {
+		this.props.handleUserInput(e.target.value);
+	}
+
+	changeLocation (city) {
 		this.props.handleUserInput(city);
 	}
+
+	
 
 	render () {
 		let that = this;
 		return (
-			<ul className="city-list">
-				<li className="city-heading">Around the world</li>
-				{this.props.list.map(city => {
-					return <li onClick={() => that.changeLocation(city)}>{ city }</li>
-				}) }
-			</ul>
+			<div>
+				<select className="city-list-select" onChange={ this.changeLocationSelect.bind(this) }>
+					{this.props.list.map(city => {
+						return <option value={ city }>{ city }</option>
+					}) }
+				</select>
+				<ul className="city-list">
+					<li className="city-heading">Around the world</li>
+					{this.props.list.map(city => {
+						return <li onClick={() => that.changeLocation(city)}>{ city }</li>
+					}) }
+				</ul>
+			</div>
 		)
 	}
 }
